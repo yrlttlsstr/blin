93c9987e5927154f3bf31fd8c28b06339e4c3092
yrlttlsstr <95122764+yrlttlsstr@users.noreply.github.com>
2021-12-04 13:25:05 +0300

    Update check_input.h
    Add comments

--- check_input.h
+++ check_input.h
@@ -1,10 +1,11 @@
 #ifndef _CHECK_INPUT_H_
 #define _CHECK_INPUT_H_
 
-//проверка корректности данных типа double
+//ГЇГ°Г®ГўГҐГ°ГЄГ  ГЄГ®Г°Г°ГҐГЄГІГ­Г®Г±ГІГЁ Г¤Г Г­Г­Г»Гµ ГІГЁГЇГ  double
+// aaa
 double check_input_double(double& num);
 
-//проверка корректности данных типа int
+//ГЇГ°Г®ГўГҐГ°ГЄГ  ГЄГ®Г°Г°ГҐГЄГІГ­Г®Г±ГІГЁ Г¤Г Г­Г­Г»Гµ ГІГЁГЇГ  int
 int check_input_int(int& num);
 
 #endif // _CHECK_INPUT_H_
